491742ded9cb32b8923b8f1879a5541f1eedeaa3
SV3N77 <danny.jiang1234@hotmail.com>
2022-08-18 09:43:50 +1000

    fix svg

--- components/EditForm.jsx
+++ components/EditForm.jsx
@@ -158,9 +158,9 @@ export default function EditForm({
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokewidth="2"
               d="M6 18L18 6M6 6l12 12"
             ></path>
           </svg>
