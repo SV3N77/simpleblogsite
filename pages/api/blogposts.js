cbc210fa0a2801f928c0edbc9453733b5f4a5923
SV3N77 <danny.jiang1234@hotmail.com>
2022-08-18 12:44:49 +1000

    update data so that it is descending order

--- pages/api/blogposts.js
+++ pages/api/blogposts.js
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Timestamp } from "firebase-admin/firestore";
 import { db } from "../../firebase/firebase.server";
 
 export default async function handler(req, res) {
   const { method } = req; // gets request method and url
   // firebase firestore database
   const blogpostsRef = db.collection("blogposts");
-  const snapShot = await blogpostsRef.get();
+  const snapShot = await blogpostsRef.orderBy("timestamp", "desc").get();
   const blogposts = snapShot.docs.map((doc) => {
     return { id: doc.id, ...doc.data() };
   });
@@ -17,10 +18,12 @@ export default async function handler(req, res) {
     }
     case "POST": {
       const { title, content, image } = req.body;
+      const timestamp = Timestamp.fromDate(new Date()).toDate();
       const newBlogpost = {
         title,
         content,
         image,
+        timestamp,
       };
       const docRef = await blogpostsRef.add(newBlogpost);
       console.log("Document written with ID: ", docRef.id);
