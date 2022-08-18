c58b0f52e53d03e745f357d405fcbbcb840ef20b
SV3N77 <danny.jiang1234@hotmail.com>
2022-08-18 12:54:20 +1000

    update button

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ onClick, children, ...props }) {
   return (
     <button
       onClick={onClick}
-      className="rounded-lg border-2 border-orange-200 bg-orange-50 p-2 transition duration-200 hover:bg-orange-200 disabled:opacity-50"
+      className="rounded-lg border-2 border-orange-200 bg-orange-50 p-2 transition duration-200 hover:bg-orange-200 disabled:pointer-events-none disabled:opacity-50"
       {...props}
     >
       {children}
